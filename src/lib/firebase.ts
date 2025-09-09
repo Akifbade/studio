edf1a715bfd719500a8c815cf918649bba7b7b44
akif bade <acshona143@gmail.com>
2025-09-09 08:36:19 +0000

    AGAIN FIREBASE DATA GONE WHY

---
+++ src/lib/firebase.ts
@@ -0,0 +1,26 @@
+'use client';
+
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAulR2nJQm-4QtNyEqKTnnDPw-iKW92Mc",
+    authDomain: "my-job-file-system.firebaseapp.com",
+    projectId: "my-job-file-system",
+    storageBucket: "my-job-file-system.appspot.com",
+    messagingSenderId: "145307873304",
+    appId: "1:145307873304:web:d661ea6ec118801b4a136d",
+    measurementId: "G-8EHX5K7YHL"
+};
+
+// Initialize Firebase
+let app;
+if (!getApps().length) {  
+  app = initializeApp(firebaseConfig);
+}
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
